3c6dc9bd5d3c388e1a9460fc56cd40c982a72a2d
VishalA86 <vishalbtech07@gmail.com>
2026-08-02 18:26:58 +0530

    06:26 PM

--- src/test/java/utilities/DriverFactory.java
+++ src/test/java/utilities/DriverFactory.java
@@ -33,6 +33,6 @@ public class DriverFactory
 //        driver = new ChromeDriver();
 //        driver.manage().window().maximize();
 //        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-//        return driver;
+//        return driver;ma
     }
 }
