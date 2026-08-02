a205c0099fc772aa4ff0584cc33c023b0ad96be8
VishalA86 <vishalbtech07@gmail.com>
2026-08-02 17:36:13 +0530

    05:36 PM

--- src/test/java/utilities/DriverFactory.java
+++ src/test/java/utilities/DriverFactory.java
@@ -6,6 +6,7 @@ import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class DriverFactory 
 {
@@ -14,9 +15,22 @@ public class DriverFactory
     public static WebDriver initializeDriver() 
     {
         WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--disable-gpu");
+
+        driver = new ChromeDriver(options);
+
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         return driver;
+        
+//========== Old Default    ===============
+//        driver = new ChromeDriver();
+//        driver.manage().window().maximize();
+//        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+//        return driver;
     }
 }
